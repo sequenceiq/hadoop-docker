d89d2b336af7f4ea08d0b2563fc2e6d4e67b2164
lpuskas <laszlo.puskas@sequenceiq.com>
2014-03-27 14:37:57 +0100

    Added support for additional classpath

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,10 @@
 $HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
 
 rm /tmp/*.pid
+
+# installing libraries if any - (resource urls added comma separated to the ACP system variable)
+cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
+
 service sshd start
 $HADOOP_PREFIX/sbin/start-dfs.sh
 $HADOOP_PREFIX/sbin/start-yarn.sh
