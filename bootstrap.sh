2e032e2fc6fd15ceb6a65ac0bbe8b7071d382c98
Attila Kanto <attila.kanto@gmail.com>
2014-07-18 12:01:59 +0200

    Replace core-site.xml with a template

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ rm /tmp/*.pid
 cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
 
 # altering the core-site configuration
-sed -i.bak  s/localhost/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml
+sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
 
 service sshd start
 $HADOOP_PREFIX/sbin/start-dfs.sh
