b769f609db2fe0abdd72c6dc84513e54b075d79d
Anton Kalyaev <a.kalyaev@fun-box.ru>
2016-05-20 18:08:11 +0400

    start MapReduce JobHistory daemon

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,6 +16,7 @@ sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > 
 service sshd start
 $HADOOP_PREFIX/sbin/start-dfs.sh
 $HADOOP_PREFIX/sbin/start-yarn.sh
+$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
